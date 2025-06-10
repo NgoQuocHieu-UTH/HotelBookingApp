726fa31f067e6d65e78e18983469d13a988acf2b
NgoQuocHieu-UTH <2251120416@ut.edu.vn>
2025-06-10 23:41:52 +0700

    feat:add retrofit libs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,9 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.retrofit)//retrofit
+    implementation(libs.converter.gson)//json
+    implementation(libs.androidx.navigation.compose) //navigation
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
